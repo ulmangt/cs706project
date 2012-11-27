e0de6c41b49cffaf83a029104c373b8d9d62e18f
Geoffrey Ulman <ulman@metsci.com>
2012-11-26 20:34:49 -0500

    histogram_test.c is compiling with opengl

--- src/main/c/histogram_test.c
+++ src/main/c/histogram_test.c
@@ -3,8 +3,13 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <GL/glut.h>
 
 int main( int argc, char** argv )
 {
     printf("CUDA Histogram Calculator\n");
+
+    glEnable( GL_TEXTURE_2D );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+    glEnable( GL_BLEND );
 }
