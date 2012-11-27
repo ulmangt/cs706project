9650caf0043a45b051e9a07cdd74ea408531f8f8
Geoffrey Ulman <ulman@metsci.com>
2012-11-26 20:39:10 -0500

    histogram_test.c is compiling with glut

--- src/main/c/histogram_test.c
+++ src/main/c/histogram_test.c
@@ -9,6 +9,9 @@ int main( int argc, char** argv )
 {
     printf("CUDA Histogram Calculator\n");
 
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
+
     glEnable( GL_TEXTURE_2D );
     glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
     glEnable( GL_BLEND );
