4112de4bbed5f9d773719f45e77e6a7fa769b0c6
Geoffrey Ulman <ulman@metsci.com>
2012-11-26 21:17:48 -0500

    recreated texture data in c

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ histogram_test: histogram_test.o HistogramTextureKernel.o
 	nvcc -lglut -lGL -lcuda -lcudart -lm target/c/histogram_test.o target/c/HistogramTextureKernel.o -o target/c/histogram_test
 
 histogram_test.o:
-	gcc -lglut -lGL -lcuda -lcudart -lm -c src/main/c/histogram_test.c -o target/c/histogram_test.o
+	nvcc -lglut -lGL -lcuda -lcudart -lm -c src/main/c/histogram_test.cu -o target/c/histogram_test.o
 
 HistogramTextureKernel.o:
 	nvcc -arch=sm_11 -lglut -lGL -lcuda -lcudart -lm -c src/main/java/resources/HistogramTextureKernel.cu -o target/c/HistogramTextureKernel.o
---
+++ src/main/c/histogram_test.cu
@@ -0,0 +1,99 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#include <GL/glut.h>
+
+int textureWidth = 4000;
+int textureHeight = 4000;
+
+GLuint* textureHandles;
+bool initialized = false;
+
+float* imageData;
+
+void initImageData( float* data )
+{
+    int w,h;
+
+    double pi = atan(1) * 4;
+
+    for ( w = 0; w < textureWidth; w++ )
+    {
+        for ( h = 0; h < textureHeight; h++ )
+        {
+            double x = w / ( double ) textureWidth;
+            double y = h / ( double ) textureHeight;
+
+            double r = rand() / (double) RAND_MAX;
+            data[h+w*textureHeight] = ( y * y + sin( 2 * pi * x * x ) + r ) * 100;
+        }
+    }
+}
+
+void init(void)
+{
+    glEnable( GL_TEXTURE_2D );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+    glEnable( GL_BLEND );
+
+    textureHandles = (GLuint*) malloc( sizeof( GLuint ) );
+
+    glGenTextures( 1, textureHandles );
+
+    glBindTexture( GL_TEXTURE_2D, *textureHandles );
+
+    printf("Texture %d\n", *textureHandles);
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
+
+    imageData = (float*) malloc( sizeof( float ) * textureWidth * textureHeight );
+    initImageData( imageData );
+
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_LUMINANCE32F_ARB, textureWidth, textureHeight, 0, GL_LUMINANCE, GL_FLOAT, imageData );
+}
+
+//Drawing funciton
+void draw(void)
+{
+  if ( !initialized )
+  {
+    init( );
+    initialized = true;
+  }
+
+  //Background color
+  glClearColor( 0,1,0,1 );
+  glClear( GL_COLOR_BUFFER_BIT );
+
+
+  //Draw order
+  glFlush();
+}
+
+//Main program
+int main(int argc, char **argv)
+{
+  printf("CUDA Histogram Calculator Starting...\n");
+
+  //cutilDeviceInit(argc, argv);
+  glutInit(&argc, argv);
+
+  //Simple buffer
+  glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB );
+  glutInitWindowPosition(50,25);
+  glutInitWindowSize(500,250);
+  glutCreateWindow("Green window");
+
+  //Call to the drawing function
+  glutDisplayFunc(draw);
+  glutMainLoop();
+
+  return 0;
+}
