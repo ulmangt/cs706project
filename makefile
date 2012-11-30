6b0f3f91a7413f2a0ac9348c2adb43b8e033f3fd
Geoffrey Ulman <ulman@metsci.com>
2012-11-29 20:09:16 -0500

    added simple non-cuda c test case to provide a baseline for profiling

--- makefile
+++ makefile
@@ -7,7 +7,13 @@ GENCODE_FLAGS   := $(GENCODE_SM20) $(GENCODE_SM30)
 INCLUDES        := -I /usr/local/cuda/include -I /usr/local/cuda/samples/common/inc
 LIBS            := -lglut -lGL -lcuda -lcudart -lm
 
-all: histogram_test histogram_test2
+all: histogram_test histogram_test2 histogram_test_nocuda
+
+histogram_test_nocuda: histogram_test_nocuda.o
+	gcc -lm target/c/histogram_test_nocuda.o -o target/c/histogram_test_nocuda
+
+histogram_test_nocuda.o:
+	gcc -lm -c src/main/c/histogram_test_nocuda.c -o target/c/histogram_test_nocuda.o
 
 histogram_test: histogram_test.o
 	nvcc $(INCLUDES) $(GENCODE_FLAGS) $(LIBS) target/c/histogram_test.o -o target/c/histogram_test
@@ -22,4 +28,4 @@ histogram_test2.o:
 	nvcc $(INCLUDES) $(GENCODE_FLAGS) $(LIBS) -c src/main/c/histogram_test2.cu -o target/c/histogram_test2.o
 
 clean:
-	rm target/c/histogram_test.o target/c/histogram_test target/c/histogram_test2.o target/c/histogram_test2
+	rm target/c/histogram_test.o target/c/histogram_test target/c/histogram_test2.o target/c/histogram_test2 target/c/histogram_test_nocuda.o target/c/histogram_test_nocuda
---
+++ src/main/c/histogram_test_nocuda.c
@@ -0,0 +1,90 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define numBins 10
+#define width 1000
+#define height 1000
+
+inline float clamp(float f, float a, float b)
+{
+    return fmaxf(a, fminf(f, b));
+}
+
+void initImageData( float* data )
+{
+    int w,h;
+
+    float pi = atan(1) * 4;
+
+    for ( w = 0; w < width; w++ )
+    {
+        for ( h = 0; h < height; h++ )
+        {
+            float x = w / ( float ) width;
+            float y = h / ( float ) height;
+
+            float r = rand() / (float) RAND_MAX;
+            data[h+w*height] = ( y * y + sin( 2 * pi * x * x ) + r ) * 100;
+        }
+    }
+}
+
+//Main program
+int main(int argc, char **argv)
+{
+    // size of texture data
+    unsigned int size = width * height * sizeof(float);
+
+    // allocate space for texture data and initialize with interesting function
+    float *imageData = (float*) malloc( size );
+    initImageData( imageData );
+
+    // allocate space for accumulated bin counts
+    int bins[numBins];
+    int i;
+    for ( i = 0 ; i < numBins ; i++ )
+    {
+        bins[i] = 0;
+    }
+
+    float minZ = -50;
+    float maxZ = 200;
+    float stepZ = ( maxZ - minZ ) / numBins;
+
+    clock_t start = clock();
+
+    // collate results from each gpu block on cpu
+    for ( i = 0 ; i < width * height ; i++ )
+    {
+        float data = imageData[i];
+
+        // calculate bin index
+        float stepZ = ( maxZ - minZ ) / numBins;
+        float fbinIndex = floor( ( data - minZ ) / stepZ );
+        int binIndex = (int) clamp( fbinIndex, 0, numBins-1 );
+
+        bins[binIndex] += 1;
+    }
+
+    // time the cpu step
+    clock_t diff = clock() - start;
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf( "%d millis\n", msec );
+
+    // print results
+    int sum = 0;
+    for ( i = 0 ; i < numBins ; i++ )
+    {
+        sum += bins[i];
+        printf( "%d\n", bins[i] );
+    }
+
+    printf( "sum %d\n", sum );
+
+    free( imageData );
+
+    return 0;
+}
